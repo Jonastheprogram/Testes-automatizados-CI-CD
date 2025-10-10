11639926af051dcfa0d1b73eef09d820e0af2fa0
Jao <jho_jbbantonio11@live.com>
2025-10-10 19:38:38 -0300

    versão final

--- Api.Esg.Fiap/Program.cs
+++ Api.Esg.Fiap/Program.cs
@@ -66,6 +66,13 @@ if (app.Environment.IsDevelopment())
 }
 
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    db.Database.Migrate();
+}
+
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
